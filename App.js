e44e104c992265fe0c2155b65bf5c1f6f90ee255
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-07-21 11:46:19 -0400

    built dynamic list of fruits

--- App.js
+++ App.js
@@ -1,1 +1,27 @@
-alert("welocme to js array functions");
+let fruits = [
+  'bananas',
+  'strawberries',
+  'grapes',
+  'apples',
+  'watermelon'
+];
+let section = document.querySelector('section.array-section');
+
+window.addEventListener('DOMContentLoaded', () => {
+  // removing elements
+  // adding elements
+  // replacing elements
+  // from start, middle, and end of the array
+  
+  displayContent(fruits);
+})
+
+function displayContent(data) {
+  data.forEach(item => {
+    const span = document.createElement('span');
+    const content = document.createTextNode(item);
+    span.appendChild(content);
+    span.className = "array-item";
+    section.appendChild(span);
+  });
+}
